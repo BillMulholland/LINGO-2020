c26a5d52b3359cfe12d5831557a24881297c38e2
Bill Mulholland <bill@cinq.co.uk>
2019-12-31 16:39:50 +0000

    checkin

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,33 +1,33 @@
 <template>
-  <div class="row">
-<div class="col"></div>
-<div class="col">
-<div class="login ">
-  <form>
-    <h3>Sign In</h3>
-    <div class="form-group">
-      <label for="exampleInputEmail1">Email address</label>
-      <input type="text" v-model="email" placeholder="Email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-      <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+<div class="row">
+  <div class="col"></div>
+  <div class="col">
+    <div class="login ">
+      <form>
+        <h3>Sign In</h3>
+        <div class="form-group">
+          <label for="exampleInputEmail1">Email address</label>
+          <input type="text" v-model="email" placeholder="Email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+          <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input type="password" v-model="password" placeholder="Password" class="form-control" id="exampleInputPassword1">
+          <br />
+          <button type="button" class="btn btn-primary" @click="login">Login to Linco</button>
+          <br />
+          <small>You don't have an account ? You can <router-link to="/sign-up">create one</router-link>
+          </small>
+        </div>
+
+
+
+      </form>
+
     </div>
-    <div class="form-group">
-      <label for="exampleInputPassword1">Password</label>
-    <input type="password" v-model="password" placeholder="Password" class="form-control" id="exampleInputPassword1">
-    <br>
-      <button type="button" class="btn btn-primary" @click="login">Login to Linco</button>
-      <br/>
-      <p>You don't have an account ? You can <router-link to="/sign-up">create one</router-link>
-      </p>
-      </div>
 
-
-
-  </form>
-
-</div>
-
-</div>
-<div class="col"></div>
+  </div>
+  <div class="col"></div>
 </div>
 </template>
 
